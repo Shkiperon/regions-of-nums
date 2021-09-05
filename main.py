0c9217c7b7fed7f98b3e093f93ee03fe7579dc07
Shkiper <shkiperon@gmail.com>
2021-09-05 20:24:45 +0500

    Preparations for 3 SQL-tables or JSON-docs
    Author: Shkiperon <shkiperon@gmail.com>

--- main.py
+++ main.py
@@ -14,14 +14,29 @@ phones_csv_dict = {
 
 
 def get_lists_of_phones(numstype: str = 'def'):
-    if numstype not in phones_csv_dict.keys():
-        return (False, {})
-    html_body = requests.get(rossvyaz_url, verify=False).text
-    soup_body = BeautifulSoup(html_body, 'lxml')
-    phones_csv_url = soup_body.find('a', text=phones_csv_dict.get(numstype)).get('href')
-    phones_csv_file = requests.get(phones_csv_url, verify=False).text
-    phones_csv_file.pop(0)
-    print(phones_csv_file)
-    return (True, {})
+    result_lst = []
+    if numstype in phones_csv_dict.keys():
+        html_body = requests.get(rossvyaz_url, verify=False).text
+        soup_body = BeautifulSoup(html_body, 'lxml')
+        phones_csv_url = soup_body.find('a', text=phones_csv_dict.get(numstype)).get('href')
+        phones_csv_file = requests.get(phones_csv_url, verify=False).text
+        phones_lst = phones_csv_file.split('\n')
+        phones_keys = phones_lst.pop(0).split(';')
+        for iter_lst in map(lambda s: s.split(';'), phones_lst):
+            result_lst.append(dict(map(lambda lst, kv: (kv, lst), iter_lst, phones_keys)))
+    return result_lst
 
-get_lists_of_phones()
+
+def_phones_lst = get_lists_of_phones()
+pref_set = set()
+oper_set = set()
+region_set = set()
+for phone_dict in def_phones_lst:
+    pref_set.add(phone_dict['АВС/ DEF'])
+    oper_set.add(phone_dict['Оператор'])
+    region_set.add(phone_dict['Регион'])
+print(pref_set)
+print('--------')
+print(oper_set)
+print('--------')
+print(region_set)
