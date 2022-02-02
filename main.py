5d253cb81651529f109502c23e6c71cf243bf88f
Shkiper <shkiperon@gmail.com>
2022-02-02 21:45:32 +0500

    Prepare of the filling regions structure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 import requests
 import re
+import json
 from bs4 import BeautifulSoup
 
 
@@ -11,6 +13,13 @@ phones_csv_dict = {
     'abc8': 'https://digital.gov.ru/uploaded/files/abc-8xx',
     'def': 'https://digital.gov.ru/uploaded/files/def-9xx',
 }
+dict_of_regions_normalizer_path = './regions_normalizer.json'
+try:
+    with open(dict_of_regions_normalizer_path) as f:
+        dict_of_regions_normalizer = json.load(f)
+except IOError:
+    dict_of_regions_normalizer = {}
+    print('No saved file with. It will be long day :-(')
 
 
 def get_soup_body(base_url: str):
@@ -37,44 +46,51 @@ def get_lists_of_phones(numstype: str = 'def'):
 
 
 def get_list_of_regions():
+    regions_dict = {}
     soup_body = get_soup_body(fns_base_url)
     options = soup_body.find('select', {'id': 'ctl00_ctl00_ddlRegion_firstpage'})
     for i in options.find_all('option'):
         region_code, region_name = re.split(r' ', i.string, maxsplit=1)
         if re.match(r'\d{2}', region_code):
-            print(f"Code: '{region_code}'; Name: '{region_name.strip().replace('  ', ' ')}'")
+            regions_dict.update({region_code: {'region_name_fns': region_name.strip().replace('  ', ' ')}})
         else:
             print(f'Bad region code: {region_code}')
+    return regions_dict
 
 
 def_phones_lst = get_lists_of_phones()
-pref_set = set()
-oper_set = set()
 region_set = set()
 for phone_dict in def_phones_lst:
-    pref_set.add(phone_dict['АВС/ DEF'])
-    oper_set.add(phone_dict['Оператор'])
     region_set.add(phone_dict['Регион'])
-print(pref_set)
-print(len(pref_set))
-print('--------')
-print(oper_set)
-print(len(oper_set))
-print('--------')
-print(region_set)
-print(len(region_set))
-print('--------')
-get_list_of_regions()
+result_dict = get_list_of_regions()
+for region_name_mincifri in region_set:
+    if region_name_mincifri not in dict_of_regions_normalizer.keys():
+        try:
+            region_int_code = int(input(f"Please enter region code for '{region_name_mincifri}': "))
+            region_code = f'{region_int_code:02d}'
+            region_dict_from_result = result_dict.get(region_code, None)
+            updated_list = region_dict_from_result.get('region_names_mincifri_list', [])
+            updated_list.append(region_name_mincifri)
+            region_dict_from_result.update({'region_names_mincifri_list': updated_list})
+            result_dict.update({region_code: region_dict_from_result})
+            dict_of_regions_normalizer.update({region_name_mincifri: region_code})
+            print(dict_of_regions_normalizer)
+        except ValueError:
+            print('Bad region code. Sorry, but we cannot do next steps')
+            sys.exit(1)
+
+
 #TODO Need to add normalizer for names of regions in regions_set
-#TODO Need to add working FIAS database (think about importing this data from GAR format to ClickHouse and work through it)
-#TODO Add links between names of regions and terriroty codes
 
 #Final goal - output of script must be JSON in format:
 #{
-#    "start_number": "9001400000",
-#    "range_length": "5",
-#    "operator_name_mincifri": "ООО T2 Мобайл",
-#    "region_name_mincifri": "г. Москва * Московская область",
-#    "region_name_fns": "Город Москва",
-#    "region_code_fns": "77",
+#   ......
+#   "77": {
+#           "start_number": "9001400000",
+#           "range_length": "5",
+#           "operator_name_mincifri": "ООО T2 Мобайл",
+#           "region_name_mincifri": "г. Москва * Московская область",
+#           "region_name_fns": "Город Москва"
+#       },
+#   ......
 #}
